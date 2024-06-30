8a4db9ff124bb1d0300c054e4908541caab70deb
dev-blackbird-tech <brianblackbird7@gmail.com>
2024-06-30 07:45:34 +0100

    Add files via upload

--- UK/solar.js
+++ UK/solar.js
@@ -1,30 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('Navbar'); // Get the navbar by ID
-    const navLinks = document.querySelectorAll('.nav-link'); // Get all navigation links
-    const navBtb = document.querySelector('.btb'); // Get all navigation
-
-
-    window.addEventListener('scroll', function () {
-        // Check the scroll position
-        if (window.scrollY > 10) { // Trigger the color change after scrolling more than 10 pixels
-            navbar.classList.add('nav-scrolled');
-            navLinks.forEach(function (links) {
-                links.classList.add('black-text');
-            })
-
-            navBtb.classList.add('btb-change')
-
-        } else {
-            navbar.classList.remove('nav-scrolled');
-            navLinks.forEach(function (links) {
-                links.classList.remove('black-text');
-            })
-
-            navBtb.classList.remove('btb-change')
-        }
-    });
-});
-
 const translationDistance = 40; // Adjust the distance of translation
 const translatingImage = document.querySelector('#translatingImage');
 
